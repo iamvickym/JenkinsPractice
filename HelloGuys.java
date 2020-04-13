ea9dcac66e460ee936782973b285a8b72f97e9a9
iamvickym <51700161+iamvickym@users.noreply.github.com>
2020-04-13 14:18:52 +0530

    Add files via upload
    This is my hello guys prog

---
+++ HelloGuys.java
@@ -0,0 +1,10 @@
+public class HelloGuys {
+
+public static void main(String[]args){
+
+	System.out.println("Hey Mani");
+	System.out.println("Hey Riya");
+	System.out.println("Hey Duck");
+	System.out.println("Hey Arya");
+}
+}
