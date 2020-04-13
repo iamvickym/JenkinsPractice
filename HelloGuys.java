c6f24b8796f7e5e5ebef43ce4045dac42ae5e233
iamvickym <51700161+iamvickym@users.noreply.github.com>
2020-04-13 15:30:09 +0530

    Update HelloGuys.java

--- HelloGuys.java
+++ HelloGuys.java
@@ -6,5 +6,6 @@ public static void main(String[]args){
 	System.out.println("Hey Riya");
 	System.out.println("Hey Duck");
 	System.out.println("Hey Arya");
+	System.out.println("Hey Surya");//newly added
 }
 }
